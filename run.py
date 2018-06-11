9a4d95669609b11ace6ed7ed31470570cb647edf
kmussar <TheGreenSpleen25@gmail.com>
2018-06-11 16:40:00 -0700

    Add files via upload

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ Created on Thu May 31 19:26:18 2018
 @author: kmuss
 """
 
-from markov_python.cc_markov import MarkovChain
+from markov_python.cc_markov_ryan import MarkovChain
 mc = MarkovChain()
 
 mc.add_file('marvel_storylines_from_urls.txt')
 
-story = mc.generate_text(20)
+story = mc.generate_text(30)
 story = ' '.join(story)
 
 print('Next from the Marvel Cinematic Universe: ')
-print(story + '.')
+print(story + '...')
